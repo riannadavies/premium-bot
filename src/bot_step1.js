7dbc5ae1606838d83cd387a46c92d4713e25e6e5
riannadavies <75624629+riannadavies@users.noreply.github.com>
2020-12-08 01:20:31 +1100

    Update bot_step1.js

--- src/bot_step1.js
+++ src/bot_step1.js
@@ -1,8 +1,8 @@
 const eris = require('eris');
-const { BOT_TOKEN } = require('../config.json');
+const { Nzg1NTA2NjQ2ODAxMzgzNDU0.X842Eg.N1CxzaiKBYTcfvlgWC57PXOcx6Q } = require('../config.json');
 
 // Create a Client instance with our bot token.
-const bot = new eris.Client(BOT_TOKEN);
+const bot = new eris.Client(Nzg1NTA2NjQ2ODAxMzgzNDU0.X842Eg.N1CxzaiKBYTcfvlgWC57PXOcx6Q);
 
 // When the bot is connected and ready, log to console.
 bot.on('ready', () => {
@@ -11,7 +11,7 @@ bot.on('ready', () => {
 
 // Every time a message is sent anywhere the bot is present,
 // this event will fire and we will check if the bot was mentioned.
-// If it was, the bot will attempt to respond with "Present".
+// If it was, the bot will attempt to respond with "hi".
 bot.on('messageCreate', async (msg) => {
   const botWasMentioned = msg.mentions.find(
     mentionedUser => mentionedUser.id === bot.user.id,
@@ -19,7 +19,7 @@ bot.on('messageCreate', async (msg) => {
 
   if (botWasMentioned) {
     try {
-      await msg.channel.createMessage('Present');
+      await msg.channel.createMessage('hi');
     } catch (err) {
       // There are various reasons why sending a message may fail.
       // The API might time out or choke and return a 5xx status,
